ce2b723076126dcef1a460663d17b7f49d61218d
“Masum” <masumkhalil7@gmail.com>
2018-07-29 07:36:14 -0700

    Git er ma re Chuddi

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Your name & AGE");
+            string name=Console.ReadLine();
+            int age = Console.Read();
+            Console.WriteLine(name+age);
+            Console.ReadKey();
+        }
+    }
+}
